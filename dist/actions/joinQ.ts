b5ec35010042bcdd23454525f6eafd24eb2c1888
Austin Caron <aus10caron@icloud.com>
2019-11-10 17:09:39 -0500

    checks if player is in queue

--- dist/actions/joinQ.ts
+++ dist/actions/joinQ.ts
@@ -1,5 +1,25 @@
-//msg.reply(`Added ${msg.author.username} to the queue!`);
-export default function(msg) {
-  msg.reply(`Added ${msg.author.username} to the queue!`);
-  console.log("Added user to queue");
+import { Message } from "discord.js";
+import Member from "../classes/member";
+
+const queueOfPlayers: Array<Member> = [];
+
+/**
+ *
+ * @param user User to add to the queue
+ */
+export default function(user: Member, msg: Message): void {
+  let { username, id } = user;
+
+  // Check if player is in queue
+  let playerAlreadyInQueue = queueOfPlayers.some(
+    user => user.username === username && user.id === id
+  );
+
+  // If player isn't in que, add them to it
+  if (!playerAlreadyInQueue) {
+    queueOfPlayers.push(user);
+    msg.reply(`we added you to the queue!`);
+  } else {
+    msg.reply(`you're already in the queue!`);
+  }
 }
