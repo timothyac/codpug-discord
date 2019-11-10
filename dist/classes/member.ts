995e8371ba857b204816a84deaeff2f98d27c83e
Austin Caron <aus10caron@icloud.com>
2019-11-10 17:08:57 -0500

    user creation and checking

---
+++ dist/classes/member.ts
@@ -0,0 +1,11 @@
+export default class Member {
+  username: string;
+  id: string;
+  elo: number = 50;
+  inQueue: boolean = false;
+
+  constructor(username: string, id: string) {
+    this.username = username;
+    this.id = id;
+  }
+}
