86add9917573ebefd6c5e6e60da23ce441fb48ba
Austin Caron <aus10caron@icloud.com>
2019-11-10 17:10:03 -0500

    check for user, new leaderboard cmd

--- dist/index.ts
+++ dist/index.ts
@@ -1,15 +1,20 @@
 // .env file where you will store your discord bot token
 require("dotenv").config();
-import * as Discord from "discord.js";
+import { Client } from "discord.js";
+import checkOrCreateUser from "./checkOrCreateUser";
 import joinQueue from "./actions/joinQ";
 
-const client = new Discord.Client();
+const client = new Client();
 
 client.on("ready", () => console.log(`Logged in as ${client.user.tag}!`));
 
-client.on("message", msg => {
-  if (msg.content === "!joinQ") {
-    joinQueue(msg);
+client.on("message", async message => {
+  if (message.content === "!joinQ") {
+    let user = await checkOrCreateUser(message);
+    await joinQueue(user, message);
+  }
+
+  if (message.content === "!leaderBoard") {
   }
 });
 
