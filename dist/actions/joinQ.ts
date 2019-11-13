c9e57862d6a4d91df26b629c9469a41ad4d3f863
Austin Caron <aus10caron@icloud.com>
2019-11-12 22:05:12 -0500

    more demo players + more comments

--- dist/actions/joinQ.ts
+++ dist/actions/joinQ.ts
@@ -1,17 +1,27 @@
 import { Message } from "discord.js";
 import Player from "../classes/player";
 
-let player1 = new Player({ username: "a", id: "123", elo: 90, inQueue: true });
-let player2 = new Player({ username: "b", id: "234", elo: 160, inQueue: true });
+let player1 = new Player({ username: "a", id: "123", elo: 190, inQueue: true });
+let player2 = new Player({ username: "b", id: "234", elo: 150, inQueue: true });
 let player3 = new Player({ username: "c", id: "345", elo: 250, inQueue: true });
+let player4 = new Player({ username: "d", id: "456", elo: 100, inQueue: true });
+let player5 = new Player({ username: "3", id: "567", elo: 110, inQueue: true });
 
-const queueOfPlayers: Array<Player> = [player1, player2, player3];
+const queueOfPlayers: Array<Player> = [
+  player1,
+  player2,
+  player3,
+  player4,
+  player5
+];
 
 /**
  *
  * @param player to add to the queue
+ * @param msg so you can reploy to the user
+ * @returns {Array} queueOfPlayers
  */
-export default function(player: Player, msg: Message) {
+export default function(player: Player, msg: Message): Array<Player> {
   let { username, id } = player;
 
   // Check to see if player is already in queue
@@ -27,6 +37,5 @@ export default function(player: Player, msg: Message) {
     msg.reply(`we added you to the queue!`);
   }
 
-  console.log(queueOfPlayers);
   return queueOfPlayers;
 }
