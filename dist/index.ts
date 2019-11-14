18004c7b65af532d5a08d639f90b4299d86d2cdd
Austin Caron <aus10caron@icloud.com>
2019-11-13 22:37:03 -0500

    building rich embed after matched players

--- dist/index.ts
+++ dist/index.ts
@@ -1,6 +1,6 @@
 // .env file where you will store your discord bot token
 require("dotenv").config();
-import { Client } from "discord.js";
+import { Client, RichEmbed } from "discord.js";
 import checkOrCreatePlayer from "./modules/checkOrCreatePlayer";
 import matchPlayers from "./modules/matchPlayers";
 import joinQueue from "./actions/joinQ";
@@ -17,7 +17,41 @@ client.on("message", async message => {
   if (message.content === "!joinQ") {
     let activeQueue = await joinQueue(player, message);
     // Match players
-    await matchPlayers(player, activeQueue);
+    let { foundMatch, matchedPlayer } = await matchPlayers(player, activeQueue);
+
+    // Determine if the match was found
+    if (foundMatch) {
+      // Create a rich discord embed
+      let richEmbed = new RichEmbed();
+
+      // Set embed properties
+      richEmbed.setColor(3140255);
+      richEmbed.setTitle(
+        `${player.username}(${player.elo}) vs. ${matchedPlayer.username}(${matchedPlayer.elo})`
+      );
+      richEmbed.setDescription("Best of 3. S&D. CDL Ruleset.");
+      richEmbed.addField("Map 1", `Arklov Peak [${player.username}]`, true); // TO-DO: Generate maps randomly
+      richEmbed.addField(
+        "Map 2",
+        `Gun Runner [${matchedPlayer.username}]`,
+        true
+      );
+      richEmbed.addField(
+        "Tie Breaker",
+        `St. Pretrograd [${player.username}]`,
+        true
+      );
+      richEmbed.setFooter("codpug discord bot - v0.1.0"); // TO-DO: Pull correct bot version
+      richEmbed.setTimestamp();
+
+      // Send in the channel the original message was set, can be adjusted
+      message.channel.send(richEmbed);
+    } else {
+      // Reply that we couldn't find a match right now
+      message.reply(
+        "no match found at this time. We will keep trying to match you with other players"
+      );
+    }
   }
 
   // Check Leaderboard
