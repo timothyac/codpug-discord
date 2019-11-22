02df3377d374c755ab694f6641b446185ac963c4
Austin Caron <aus10caron@icloud.com>
2019-11-22 15:26:17 -0500

    updating interface

--- dist/classes/alertData.ts
+++ dist/classes/alertData.ts
@@ -1,8 +1,10 @@
 import { TextChannel } from "discord.js";
 import Player from "./player";
+import Queue from "./queue";
 
 export default interface alertData {
   player1: Player;
   player2: Player;
   channel: TextChannel;
+  queue: Queue;
 }
